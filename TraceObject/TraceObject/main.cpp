5b497c05d46a3abf1fa1867209b49e07d2dce5ba
Daniel Henderson <tindrum@mac.com>
2013-11-25 19:53:55 -0800

    First commit. Project created in AppCode

---
+++ TraceObject/TraceObject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TraceObject
+//
+//  Created by Daniel Henderson on 11/25/13.
+//  Copyright (c) 2013 ___Dan___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
